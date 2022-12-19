3023dd40126a00969b50bd2fd49425eafcabc66a
Aman Sagar <amansagar887@gmail.com>
2022-12-19 08:25:25 +0530

    seven

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -4,9 +4,12 @@ public class Boat implements WaterVehicle{
     private String name;
     private int capacity;
 
-    public Boat(String name,int caacity){
+    public Boat(String name,int capacity){
         this.name = name;
         this.capacity = capacity;
+    }
+    public Boat(){
+
     }
 
     @Override
